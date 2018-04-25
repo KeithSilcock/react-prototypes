ec382a0afd0ce151d752bc2e67e78b7b6c16e4c9
KeithSilcock <silcockk@gmail.com>
2018-04-25 09:37:29 -0700

    completed react intro prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getRandNumBetween(min, max){
+    let num = Math.floor(Math.random()*(max-min+1) + min);
+    return num;
+}
+
+function luckyNumber(){
+    return getRandNumBetween(1,1000);
+}
+
+var user = {
+    name: "Christopher Robin",
+    luckyNumber: luckyNumber(),
+}
+
+function greeting(user) {
+    let container = (
+        <div className='container'>
+            <h1>Hello, {user.name}! We hope you enjoy your stay!</h1>
+            <h2 className='text-muted'>Your fortune favors you! Your lucky number is: {user.luckyNumber}</h2>
+        </div>
+    )
+    return container;
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
