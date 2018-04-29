e3d073488b1181b3d5e7d746d9f895d4571c5484
Keith Silcock <silcockk@gmail.com>
2018-04-29 12:29:25 -0700

    checking out to master

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Greeting from './greeting'
 
-
+ReactDOM.render(
+    <Greeting name='Serm Skervuun'/>,
+    document.getElementById('root')
+);
---
+++ src/components/greeting.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function(props){
+    debugger;
+    return (
+        <h1>Woah woah woah, {props.name}, you're not allowed here</h1>
+    )
+}
