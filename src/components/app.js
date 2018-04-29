11e8ddbbc3addf659393b6dea36e68957dcbe848
KeithSilcock <silcockk@gmail.com>
2018-04-29 15:10:26 -0700

    finished functional components prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,23 @@
+import React from 'react';
+import Table from './table';
 
+const students=[
+    {name: "christopher Robin",
+        course: 'Math',
+        grade: 45},
+    {name: "Dorkinson Robins",
+        course: 'Cats',
+        grade: 40},
+    {name: "man mcman",
+        course: 'Man',
+        grade: 95},
+]
 
+export default () => {
+    return(
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
