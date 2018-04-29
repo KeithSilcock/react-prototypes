02f78e470af13364a88da617fc87b7a20069490e
KeithSilcock <silcockk@gmail.com>
2018-04-29 16:51:39 -0700

    created contacts display

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import ContactList from './contactList'
 
-
+export default () => {
+    return(
+        <div className="container">
+            <h1 className="text-center">Address Book</h1>
+            <div className="row">
+                <ContactList/>
+            </div>
+        </div>
+    )
+}
---
+++ src/data/contacts.js
@@ -0,0 +1,22 @@
+
+
+export default [
+    {
+        firstName: 'bob',
+        lastName:'jan',
+        phone: '9496805632',
+        email:'at@at.at',
+    },
+    {
+        firstName: 'bober',
+        lastName:'janer',
+        phone: '94968056323432',
+        email:'at@at.ater'
+    },
+    {
+        firstName: 'bob',
+        lastName:'jan',
+        phone: '9496805632',
+        email:'at@at.at'
+    },
+];
