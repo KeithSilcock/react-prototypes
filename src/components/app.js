8ee673c9bb12d8e3d9921c1871c224da2813d19c
KeithSilcock <silcockk@gmail.com>
2018-04-29 17:44:58 -0700

    started form functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ContactList from './contactList'
+import ContactForm from "./contact_form";
 
 export default () => {
     return(
         <div className="container">
             <h1 className="text-center">Address Book</h1>
             <div className="row">
+                <div className="col-4">
+                    <ContactForm/>
+                </div>
                 <ContactList/>
             </div>
         </div>
