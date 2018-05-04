1561a892d65a55913ce73fb5974398b3ebaa0373
KeithSilcock <silcockk@gmail.com>
2018-05-04 09:33:07 -0700

    finished prototype

---
+++ src/components/scrapbook_image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default (props) => {
+    const {src, style} = props.about
+
+    return(
+        <img src={src} className='scrapbook-image' style={style} alt=""/>
+    )
+}
