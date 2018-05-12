ccf317f21d25e4530ed7faddb24799f6c60b4502
KeithSilcock <silcockk@gmail.com>
2018-05-12 14:21:24 -0700

    this is a working redux setup. Use this if you need to start a redux project from scratch

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,10 @@
+import React from 'react';
 
+export default () => {
+    return(
+        <div className="container">
+            <h1 className="text-center">Clock</h1>
+        </div>
+    )
+}
 
